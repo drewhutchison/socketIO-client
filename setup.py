e6c9a3e8d1ab1a36a0c33308891f0ea1a50280ab
andrew hutchison <drew.hutchison@nimbusnine.co>
2016-08-16 17:17:44 -0600

    clamped version of requests, 2.11.0 breaks my ssl

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     author_email='rhh@crosscompute.com',
     url='https://github.com/invisibleroads/socketIO-client',
     install_requires=[
-        'requests',
+        'requests == 2.9.1',
         'six',
         'websocket-client',
     ],
